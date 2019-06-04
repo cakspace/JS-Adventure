30fbc2554490dd06a1447f6a26cc55b5bd3de503
cakspace <cakspace@gmail.com>
2019-06-03 21:25:49 -0400

    version 4...0_o

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 const toDoList = {
-  todos: ["index 1", "index 2", "index 3"],
+  todos: [],
 
   displayTodos: function() {
     console.log(this.todos);
   },
 
-  addTodos: function(todo) {
-    this.todos.push(todo);
+  addTodos: function(todoText) {
+    this.todos.push({
+      todoText: todoText,
+      completed: false
+    });
     console.log(this.todos);
   },
 
-  changeTodos: function(index, value) {
-    this.todos[index] = value;
+  changeTodos: function(index, todoText) {
+    this.todos[index].todoText = todoText;
     console.log(this.todos);
   },
 
   removeTodos: function(index) {
     this.todos.splice(index, 1);
     console.log(this.todos);
+  },
+
+  toggleCompleted: function(index) {
+    let todo = this.todos[index];
+    todo.completed = !todo.completed;
   }
 };
